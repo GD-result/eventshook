da6977f4fd7c6d412e8a46efcb2f669c58c5cb78
Dreambit <dreambitc@gmail.com>
2012-03-02 01:44:17 +0400

    Abstract

---
+++ eventshook.py
@@ -0,0 +1,28 @@
+'''
+Created on 02.03.2012
+
+@author: dreambit
+
+@organization: GridDynamics
+'''
+def getNews(githubLogin, organizationName):
+    """
+    @type githubLogin: String
+    @type organizationName: String
+    @param githubLogin: Login on GitHub
+    @param organizationName: Organization name on GitHub
+    @return: Response from Server, which contains a list of organizations in JSON format
+    """
+    pass
+def addNewsToDB():
+    """
+    Add last news from JSON content into DataBase
+    @return: None
+    """
+    pass
+def getLastNewsId():
+    """
+    This method gets the ID of the latest news from DataBase
+    @return: ID of the latest news from DataBase
+    """
+    pass
